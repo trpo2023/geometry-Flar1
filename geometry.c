726481529c8907b5e36b2a39f0268307a57d177a
Никулин Максим Кириллович <is242s13@bwp16.csc.local>
2023-03-07 13:05:24 +0700

    add errors brackets

--- geometry.c
+++ geometry.c
@@ -1,32 +1,33 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#define ascii_bracket_left 40
+#define ascii_bracket_right 41
 
 typedef struct point {
     double x;
     double y;
-}point;
+} point;
 
 typedef struct circle {
     point center;
     double radius;
-}circle;
-int error_one(char *str)
+} circle;
+int left_bracket(char* str)
 {
     int j = str[6];
-    if (j == 41)
-    {
+    if (j == ascii_bracket_right) {
         return 1;
     }
     return 0;
 }
-int erorr_two(char *str)
+int right_bracket(char* str)
 {
-    int j = 40;
-    for (int i = 9; i < 40;i++)
-    {
+    int j = ascii_bracket_left;
+    for (int i = 9; i < ascii_bracket_left; i++) {
         if (str[i] == j)
-        return 1;
+            return i + 1;
     }
     return 0;
 }
@@ -44,7 +45,7 @@ int isArguments(char* str)
     for (int i = 7; str[i] != ',' && i < strlen(str); i++) {
         if ((str[i] != '.' && str[i] != ' ')
             && !(str[i] >= 48 && str[i] <= 57)) {
-            printf("Error: expected '<double>'\n");
+            printf("Error at column %d: expected '<double>'\n",i+1);
             ret++;
             return 1;
         }
@@ -58,31 +59,8 @@ int isArguments(char* str)
         ret++;
         return ret;
     }
-    int index = 0;
-    for (int i = 0; i != strlen(str); i++) {
-        if (str[i] == ',') {
-            index = i + 1;
-            i = strlen(str) - 1;
-        }
-    }
-    for (; str[index] != ')' && index < strlen(str); index++) {
-        if ((str[index] != '.' && str[index] != ' ')
-            && !(str[index] >= 48 && str[index] <= 57)) {
-            printf("Error: expected radius '<double>'\n");
-            ret++;
-            return 1;
-        }
-        if (str[index] >= 48 && str[index] <= 57 && str[index + 1] == ' ')
-            count++;
-        if (str[index] == '.' && str[index + 1] == ' ')
-            count += 2;
-    }
-    if (count != 1) {
-        printf("Error: expected radius '<double>'\n");
-        ret++;
-    }
-    return ret;
 }
+    
 
 int isEnd(char* str)
 {
@@ -120,13 +98,14 @@ int isObject(char* str)
 
 int printErrors(char* str, int countObj)
 {
+	int z = right_bracket(str);
     printf("Position %d:\n", countObj);
     if (isObject(str))
-        printf("Error: expected 'circle'\n");
-    else if (error_one(str))
-        printf("Error: expected '('\n");
-    else if(erorr_two(str))
-        printf("Error: expected ')'\n");
+        printf("Error at column 0: expected 'circle'\n");
+    else if (left_bracket(str))
+        printf("Error at column 7: expected '('\n");
+    else if (right_bracket(str))
+        printf("Error at column %d: expected ')'\n",z);
     else if (isArguments(str))
         return 0;
     else if (isEnd(str))
@@ -140,6 +119,10 @@ int main()
 {
     FILE* file;
     file = fopen("test.txt", "r");
+    if (fopen("test.txt", "r") == NULL) {
+        printf("File not found\n");
+        exit(0);
+    }
     char str1[100];
     int countObj = 0;
     while (fgets(str1, 99, file)) {
