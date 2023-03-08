f2e8bbc4f15ef1a6110bda60e44866320fc979b4
Nikulin Maxim <Flar1@users.noreply.github.com>
2023-03-08 20:44:24 +0700

    fix line on error output

--- geometry.c
+++ geometry.c
@@ -1,19 +1,20 @@
 #include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #define ascii_bracket_left 40
 #define ascii_bracket_right 41
-
+#define zero 48
+#define nine 57
 typedef struct point {
     double x;
     double y;
-} point;
+}point;
 
 typedef struct circle {
     point center;
     double radius;
-} circle;
+}circle;
 int left_bracket(char* str)
 {
     int j = str[6];
@@ -31,7 +32,6 @@ int right_bracket(char* str)
     }
     return 0;
 }
-
 void strtolower(char* str)
 {
     for (int i = 0; i < strlen(str); i++)
@@ -42,25 +42,64 @@ int isArguments(char* str)
 {
     int ret = 0;
     int count = 0;
+    int j = 0;
+    int z = 0;
     for (int i = 7; str[i] != ',' && i < strlen(str); i++) {
         if ((str[i] != '.' && str[i] != ' ')
-            && !(str[i] >= 48 && str[i] <= 57)) {
+            && !(str[i] >= zero && str[i] <= nine)) {
             printf("Error at column %d: expected '<double>'\n",i+1);
+            j = i+1;
             ret++;
             return 1;
         }
-        if (str[i] >= 48 && str[i] <= 57 && str[i + 1] == ' ')
+        if (str[i] >= zero && str[i] <= nine && str[i + 1] == ' ')
+        {
             count++;
+            j = i+1;
+        }
         if (str[i] == '.' && str[i + 1] == ')')
+        {
             count += 2;
+            j = i+1;
+        }
     }
     if (count + 1 != 2) {
-        printf("Error: expected '<double>'\n");
+        printf("Error at column %d: expected '<double>'\n",j);
         ret++;
         return ret;
     }
+    int index = 0;
+    for (int i = 0; i != strlen(str); i++) {
+        if (str[i] == ',') {
+            index = i + 1;
+            i = strlen(str) - 1;
+        }
+    }
+    for (; str[index] != ')' && index < strlen(str); index++) {
+        if ((str[index] != '.' && str[index] != ' ')
+            && !(str[index] >= 48 && str[index] <= 57)) {
+            printf("Error at column %d: expected radius '<double>'\n",index+1);
+            z = index+1;
+            ret++;
+            return 1;
+        }
+        if (str[index] >= 48 && str[index] <= 57 && str[index + 1] == ' ')
+        {
+            count++;
+            z = index+1;
+        }
+        if (str[index] == '.' && str[index + 1] == ' ')
+        {
+            count += 2;
+            z = index+1;
+        }
+    }
+    if (count != 1) {
+        printf("Error at column %d: expected radius '<double>'\n",z);
+        ret++;
+    }
+    return ret;
 }
-    
 
 int isEnd(char* str)
 {
@@ -81,6 +120,19 @@ int isEnd(char* str)
         ret = 0;
     return ret;
 }
+int issEnd(char* str)
+{
+    long int endingSymbol;
+    if (str[strlen(str) - 1] == '\n')
+        endingSymbol = strlen(str) - 2;
+    else
+        endingSymbol = strlen(str) - 1;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == ')') {
+        }
+    }
+    return endingSymbol;
+}
 
 int isObject(char* str)
 {
@@ -99,6 +151,7 @@ int isObject(char* str)
 int printErrors(char* str, int countObj)
 {
 	int z = right_bracket(str);
+    int s = issEnd(str) - 1;
     printf("Position %d:\n", countObj);
     if (isObject(str))
         printf("Error at column 0: expected 'circle'\n");
@@ -109,7 +162,7 @@ int printErrors(char* str, int countObj)
     else if (isArguments(str))
         return 0;
     else if (isEnd(str))
-        printf("Error: unexpected token\n");
+        printf("Error at column %d: unexpected token\n",s);
     else
         printf("%s\n", str);
     return 0;
