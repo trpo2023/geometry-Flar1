637ba40301a8c296fd0617a07c873c2e05cf6235
Nikulin Maxim <Flar1@users.noreply.github.com>
2023-04-18 22:53:24 +0700

    use new function and fix codestyle

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -1,12 +1,14 @@
-#include <circle.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "circle.h"
+
 int main()
 {
     FILE* file;
     file = fopen("./data.txt", "r");
-    if (fopen("./data.txt", "r") == NULL) {
+    if (file == NULL) {
         printf("File not found\n");
         exit(0);
     }
@@ -15,26 +17,10 @@ int main()
 
     while (fgets(str1, 99, file)) {
         countObj++;
-        strtolower(str1);
-        int v = printErrors(str1, countObj);
+        str_to_lower(str1);
+        int v = print_errors(str1, countObj);
         for (; v < 1; v++) {
-            char radius[10];
-            for (int i = 7; i != strlen(str1); i++) {
-                if (str1[i] == ',') {
-                    int j = 0;
-                    i = i + 1;
-                    for (int z = i; str1[z] != ')'; z++) {
-                        radius[j] = str1[z];
-                        j++;
-                    }
-                }
-            }
-            double rad = atof(radius);
-            double per = 2 * M_PI * rad;
-            double plosh = M_PI * rad * rad;
-            printf(" perimetr = %.4lf\n", per);
-            printf(" area = %.4lf\n", plosh);
-            zeero(radius);
+            calc_per_and_area(str1);
         }
     }
     fclose(file);
