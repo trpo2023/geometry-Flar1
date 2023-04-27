7ecf388868ab7b09f2ae1cb0e2ea0cf7f1b26ab1
Nikulin Maxim <Flar1@users.noreply.github.com>
2023-04-27 20:53:35 +0700

    Fix codestyle

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -6,20 +6,23 @@
 
 int main()
 {
-    FILE* file;
+    FILE *file;
     file = fopen("./data.txt", "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("File not found\n");
         exit(0);
     }
     char str1[100];
     int countObj = 0;
 
-    while (fgets(str1, 99, file)) {
+    while (fgets(str1, 99, file))
+    {
         countObj++;
         str_to_lower(str1);
         int v = print_errors(str1, countObj);
-        for (; v < 1; v++) {
+        for (; v < 1; v++)
+        {
             calc_per_and_area(str1);
         }
     }
