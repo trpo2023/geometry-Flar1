5c1732f63d4c421a6b11087b3e6b9a27bbc9631f
Nikulin Maxim <Flar1@users.noreply.github.com>
2023-04-29 02:10:04 +0700

    add new function to CTEST

--- test/test.c
+++ test/test.c
@@ -2,6 +2,7 @@
 #include <ctest.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 CTEST(INPUT_CHECK, LEFT_BRACKET_INDEX)
 {
@@ -48,5 +49,38 @@ CTEST(INPUT_CHECK, ERROR_CHECKING)
     char str[] = "circle(0 0, 1.5)";
     int expec = 0;
     int real = print_errors(str, 1);
+    ASSERT_EQUAL(expec, real);
+}
+
+CTEST(INPUT_CHECK, CHECK_GET_PER)
+{
+    char str[] = "circle(0 0, 1.5)";
+    int expec = 9.424778;
+    int real = get_per(str);
+    ASSERT_EQUAL(expec, real);
+}
+
+CTEST(INPUT_CHECK, CHECK_GET_AREA)
+{
+    char str[] = "circle(0 0, 1.5)";
+    int expec = 7.068583;
+    int real = get_area(str);
+    ASSERT_EQUAL(expec, real);
+}
+
+CTEST(INPUT_CHECK, CHECK_GET_CENTER)
+{
+    char str[] = "circle(0 0, 1.5)";
+    circle* circle = calloc(1, sizeof(circle));
+    int expec = 0;
+    int real = get_center(str,circle);
+    ASSERT_EQUAL(expec, real);
+}
+CTEST(INPUT_CHECK, CHECK_GET_RAD)
+{
+    char str[] = "circle(0 0, 1.5)";
+    circle* circle = calloc(1, sizeof(circle));
+    int expec = 0;
+    int real = get_radius(str,circle);
     ASSERT_EQUAL(expec, real);
 }
